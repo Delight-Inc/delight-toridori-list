dc95640372efca734ead5acf3f48d9d6cac24613
YagiRyu <ryucode822@gmail.com>
2023-12-27 10:39:24 +0900

    データモデル作って、食べログから情報を取得してモデルに格納するところまでできた

---
+++ instalist.py
@@ -0,0 +1,11 @@
+import dataclasses
+
+@dataclasses.dataclass
+class Instalist:
+    shop_name: str
+    tel_number: str
+    address: str
+    instagram_link: str
+    genre: str
+    business_hours: str
+    opened_date: str
--- taberogu_search.py
+++ taberogu_search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import instalist
 
 class Taberogu:
     def __init__(self) -> None:
@@ -9,12 +10,35 @@ class Taberogu:
         self.driver.get('https://tabelog.com/tokyo')
         assert '食べログ' in self.driver.title
 
-    def search(self) -> list[str]:
+    def search(self) -> list[instalist.Instalist]:
         shop_list = self.driver.find_elements(By.CLASS_NAME, "list-rst__rst-name-target")
 
         shop_link_list = []
         for shop in shop_list:
             shop_link_list.append(shop.get_attribute('href'))
 
-        return shop_link_list
-    
+        instagram_link_list = []
+        for link in shop_link_list:
+            self.driver.get(link)
+            instagram_link = self.driver.find_elements(By.CLASS_NAME, "rstinfo-sns-link")
+            if len(instagram_link) > 0 and 'instagram' in instagram_link[0].get_attribute('href'):
+                shop_name = self.driver.find_element(By.CLASS_NAME, "rstinfo-table__name-wrap").text
+                tel_number = self.driver.find_element(By.CLASS_NAME, "rstinfo-table__tel-num").text
+                address = self.driver.find_element(By.CLASS_NAME, "rstinfo-table__address").text
+                genre = self.driver.find_element(By.CLASS_NAME, "rdheader-subinfo__item-text").text
+                business_hours = self.driver.find_element(By.CLASS_NAME, "rstinfo-table__subject-text").text
+                opened_date = self.driver.find_element(By.CLASS_NAME, "rstinfo-opened-date").text
+
+                instagram_link_list.append(
+                    instalist.Instalist(
+                        shop_name,
+                        tel_number,
+                        address,
+                        instagram_link[0].get_attribute('href'),
+                        genre,
+                        business_hours,
+                        opened_date
+                    )
+                )
+
+        return instagram_link_list
